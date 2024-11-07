5c7fdf040eed098886f2a9a029003f3f85342ff8
orhan arda duman <68629230+orhanardaduman@users.noreply.github.com>
2024-11-08 01:03:27 +0300

    file try

--- src/modules/files/files.controller.ts
+++ src/modules/files/files.controller.ts
@@ -3,6 +3,7 @@ import { Response } from 'express';
 import * as path from 'path';
 import * as fs from 'fs';
 import { Public } from '../../helpers/make-public';
+import * as mime from 'mime-types';
 
 @Controller('file')
 export class FilesController {
@@ -23,11 +24,17 @@ export class FilesController {
     console.log(imageFullPath);
 
     if (fs.existsSync(imageFullPath)) {
-      // Content-Disposition başlığını inline olarak ayarlıyoruz
+      // MIME türünü belirlemek için mime paketini kullan
+      const mimeType = mime.lookup(imageFullPath) || 'application/octet-stream';
+
+      // Content-Type başlığını MIME türü ile ayarla
+      res.setHeader('Content-Type', mimeType);
+      // Dosyanın tarayıcıda görüntülenmesi için Content-Disposition başlığını inline olarak ayarla
       res.setHeader('Content-Disposition', 'inline');
+
       return res.sendFile(imageFullPath);
     } else {
-      return res.status(404).send('Resim bulunamadı');
+      return res.status(404).send('Dosya bulunamadı');
     }
   }
 }
